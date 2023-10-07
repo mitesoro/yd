aedb60e22a5fc7017627f202e3f616fd52c3d81f
liuhaigang <liuhaigang@qimao.com>
2023-10-07 16:10:45 +0800

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<ydApi.h>
+#include<ydDataStruct.h>
+#include<ydDataType.h>
+#include<ydError.h>
+#include<ydUtil.h>
+
+using namespace std;
+
+int main(){
+    cout << "hello world" << endl; 
+    return 0;
+}
