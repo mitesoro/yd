c8cf1fdeefbd7061933f7db8cafaecab0a503d9d
liuhaigang <liuhaigang@qimao.com>
2023-10-11 18:29:53 +0800

    up

---
+++ cmake-build-debug-remote-host/CMakeFiles/yd_demo.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/yd_demo.dir/src/YdUserApiInfo.cpp.o"
+  "CMakeFiles/yd_demo.dir/src/YdUserApiInfo.cpp.o.d"
+  "CMakeFiles/yd_demo.dir/src/greeter.grpc.pb.cc.o"
+  "CMakeFiles/yd_demo.dir/src/greeter.grpc.pb.cc.o.d"
+  "CMakeFiles/yd_demo.dir/src/greeter.pb.cc.o"
+  "CMakeFiles/yd_demo.dir/src/greeter.pb.cc.o.d"
+  "CMakeFiles/yd_demo.dir/src/greeter_client.cc.o"
+  "CMakeFiles/yd_demo.dir/src/greeter_client.cc.o.d"
+  "CMakeFiles/yd_demo.dir/src/greeter_server.cc.o"
+  "CMakeFiles/yd_demo.dir/src/greeter_server.cc.o.d"
+  "CMakeFiles/yd_demo.dir/src/main.cpp.o"
+  "CMakeFiles/yd_demo.dir/src/main.cpp.o.d"
+  "yd_demo"
+  "yd_demo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/yd_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- cmake-build-debug-remote-host/CMakeFiles/yd_demo.dir/flags.make
+++ cmake-build-debug-remote-host/CMakeFiles/yd_demo.dir/flags.make
@@ -4,7 +4,7 @@
 # compile CXX with /usr/bin/c++
 CXX_DEFINES = 
 
-CXX_INCLUDES = -I/tmp/yd/include
+CXX_INCLUDES = -I/tmp/tmp.4gw0p9Y8So/include
 
 CXX_FLAGS = -g -std=gnu++11
 
